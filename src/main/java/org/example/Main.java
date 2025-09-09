feedaf5f8649f5de862d53bf3da5f027a23be290
Miguel Angel Tovar Muñoz <miguel_tovarmu@fet.edu.co>
2025-09-09 16:09:22 -0500

    Update Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,37 +1,3 @@
-class CuentaBancaria {
-
-    private double saldo;
-
-    public CuentaBancaria() {
-        this.saldo = 0;
-    }
-
-    public void depositar(double monto) {
-        if (monto > 0) {
-            saldo += monto;
-        } else {
-            System.out.println("El monto a depositar debe ser positivo.");
-        }
-    }
-
-    public void retirar(double monto) {
-        if (monto > 0) {
-            if (monto <= saldo) {
-                saldo -= monto;
-            } else {
-                System.out.println("Fondos insuficientes. No se puede retirar " + monto);
-            }
-        } else {
-            System.out.println("El monto a retirar debe ser positivo.");
-        }
-    }
-
-    public double getSaldo() {
-        return saldo;
-    }
-}
-
-
 public class Main {
     public static void main(String[] args) {
         CuentaBancaria cuenta = new CuentaBancaria();
